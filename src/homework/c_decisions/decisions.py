177ffc1c3fd5f6eb8d874f0c0fb491bb68e5fc10
acc-aapage <157080534+acc-aapage@users.noreply.github.com>
2024-02-08 17:44:28 +0000

    homework 2

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,19 @@
-#
+def get_options_ratio(options, total):
+    ratio = float(options / total)
+    return ratio
+
+def get_faculty_rating(ratio):
+    if ratio >= 1:
+        print("Ratio is invalid; program could not produce rating. Rating should be less than 1.")
+        rating = "Invalid"
+    elif ratio >= 0.9:
+        rating = "Excellent"
+    elif ratio >= 0.8:
+        rating = "Very Good"
+    elif ratio >= 0.7:
+        rating = "Good"
+    elif ratio >= 0.6:
+        rating = "Needs Improvement"
+    else:
+        rating = "Unacceptable"
+    return rating
--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,7 @@
-#
+import decisions
+
+options = float(input("Input Option: "))
+total = float(input("Input Total: "))
+result = decisions.get_options_ratio(options, total)
+
+print("Rating:",decisions.get_faculty_rating(result))
